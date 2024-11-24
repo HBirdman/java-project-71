fcd2442d2e9e6f36e845da6b8fe069dac0b3717d
HBirdman <hbirdman@mail.ru>
2024-11-24 18:57:27 +0300

    Added picocli help output

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,8 @@ repositories {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.6")
+    compileOnly("info.picocli:picocli-codegen:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,38 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(
+        mixinStandardHelpOptions = true,
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference."
+)
+public class App implements Runnable {
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested = false;
+
+    @Override
+    public void run() {
         System.out.println("Hello World!");
     }
+
+    public static void main(String[] args) {
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.parseArgs(args);
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+            return;
+        } else if (commandLine.isVersionHelpRequested()) {
+            commandLine.printVersionHelp(System.out);
+            return;
+        }
+        System.out.println("Hello World!");
+    }
+
 }
