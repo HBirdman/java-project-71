ba2cd4025135cca005f8338b9da12f340f5a9a8a
HBirdman <hbirdman@mail.ru>
2024-12-20 20:03:29 +0300

    Code refactoring №4

--- app/src/main/java/hexlet/code/Constant.java
+++ app/src/main/java/hexlet/code/Constant.java
@@ -10,4 +10,11 @@ public class Constant {
     public static final String STATUS_DELETED = "deleted";
     public static final String STATUS_UNCHANGED = "unchanged";
     public static final String STATUS_MODIFIED = "modified";
+    public static final String jsonFile1 = "src/test/resources/fixtures/file1.json";
+    public static final String jsonFile2 = "src/test/resources/fixtures/file2.json";
+    public static final String yamlFile1 = "src/test/resources/fixtures/file1.yml";
+    public static final String yamlFile2 = "src/test/resources/fixtures/file2.yml";
+    public static final String stylishResult = "src/test/resources/fixtures/stylishResult.txt";
+    public static final String plainResult = "src/test/resources/fixtures/plainResult.txt";
+    public static final String jsonResult = "src/test/resources/fixtures/jsonResult.txt";
 }
